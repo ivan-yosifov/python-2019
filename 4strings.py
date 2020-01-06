d5d73351e957281de05a796ae7087a82521cac54
Ivan Yosifov <ivan_yosifov@yahoo.com>
2020-01-06 15:10:41 +0200

    More commits

---
+++ 4strings.py
@@ -0,0 +1,25 @@
+########################
+# Learn Python 2019
+########################
+
+import os
+os.system('clear')
+
+greetings = "My boss yelled \"Get back to work!\""
+print(greetings)
+
+first_name = 'Megan'
+last_name = 'Fox'
+print(first_name + ' ' + last_name)
+
+print(first_name.upper())
+print(first_name.lower())
+print(first_name.capitalize())
+print(first_name.title())
+print(first_name.swapcase())
+
+print(len(first_name))
+
+print(first_name[0])
+print(first_name[0:3]) # Meg
+print(first_name.split('g')) # ['Me', 'an']
---
+++ 5numbers.py
@@ -0,0 +1,18 @@
+########################
+# Learn Python 2019
+########################
+
+import os
+os.system('clear')
+
+num = 10
+print(float(num))
+
+price = 10.25
+print(int(price))
+
+print(7 + 2)
+print(5**2)
+
+num = "5"
+print(int(num) + 9)
